556a6250fcee0197d356e00feae659fe8470248c
domipic <47837540+domipic@users.noreply.github.com>
2019-04-22 22:00:17 -0400

    updated path

--- sentiment_analysis/sentiment_analysis.py
+++ sentiment_analysis/sentiment_analysis.py
@@ -5,7 +5,7 @@ import json
 
 all_lyrics = []
 
-for root, dirs, files in os.walk("../lyrics-master/database"):
+for root, dirs, files in os.walk("../lemmatized_lyrics/database"):
     for name in files:
         if "lemmatized" in name:
             infile = open(root + "/" + name, "r")
